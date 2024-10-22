41d5738e6d6ff4136c79ee5eefdcba8a15f8c366
SucharRocketry <111312548+SucharRocketry@users.noreply.github.com>
2024-10-22 10:08:13 +0200

    version2

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <SparkFun_Qwiic_Scale_NAU7802_Arduino_Library.h>
-#include <FS.h>
+#include <Wire.h>
+#include "FS.h"
 #include <LittleFS.h>
 
-int mydata;
+#define FORMAT_LITTLEFS_IF_FAILED true
+
+bool off = false;
+unsigned long aktualny_czas = 0;
+int currentIndex = 0;
+
+
+NAU7802 myScale; //Create instance of the NAU7802 class
+
+  struct DataFrame {
+
+    unsigned long zapamietanyCzas2;
+    float nacisk;
+
+    String toString() {
+
+      char pomiar[20];
+      sprintf(pomiar, "%lu, %8.2f", zapamietanyCzas2, nacisk);
+      return String(pomiar);
+    }
+  };
+
+ DataFrame pakiecik2[12500];
+ uint8_t tempBuffer[sizeof(DataFrame)];
+
+
 
 void readFile(const char *path) {
   Serial.printf("Reading file: %s\n", path);
-
   File file = LittleFS.open(path, "r");
   if (!file) {
     Serial.println("Failed to open file for reading");
@@ -20,85 +44,106 @@ void readFile(const char *path) {
   file.close();
 }
 
-void writeFile(const char *path, const char *message) {
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = LittleFS.open(path, "w");
-  if (!file) {
-    Serial.println("Failed to open file for writing");
-    return;
+void deleteFile(fs::FS &fs, const char * path) {
+  Serial.printf("Deleting file: %s\r\n", path);
+  if (fs.remove(path)) {
+    Serial.println("- file deleted");
+  } 
+  else {
+    Serial.println("- delete failed");
   }
-  if (file.print(message)) {
-    Serial.println("File written");
-  } else {
-    Serial.println("Write failed");
-  }
-  delay(2000);  // Make sure the CREATE and LASTWRITE times are different
-  file.close();
 }
-
-void appendFile(const char *path, const char *message) {
-  Serial.printf("Appending to file: %s\n", path);
-
-  File file = LittleFS.open(path, "a");
-  if (!file) {
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if (file.print(message)) {
-
-  } else {
-    Serial.println("Append failed");
-  }
-  file.close();
-}
-
-NAU7802 myScale; //Create instance of the NAU7802 class
 
 void setup() {
+  Serial.begin(115200);
   Wire.begin();
 
-  Serial.begin(115200);
-  
-  Serial.println("Hamownia max 1kN");
-  
-  Serial.println("Mount LittleFS");
-  if (!LittleFS.begin()) {
-    Serial.println("LittleFS mount failed");
-    return;
+  pinMode(6, OUTPUT);
+
+  Serial.println("NAU7802");
+  if (! myScale.begin()) {
+    Serial.println("Failed to find NAU7802");
   }
-  else{
-    Serial.println("Little FS Mounted Successfully");
+  Serial.println("Found NAU7802");
+
+    if (!LittleFS.begin()) {
+    Serial.println("LITTLEFS Mount Failed");
+    off = true;
   }
-    
-    if (myScale.begin() == false)
+
+   
+  delay(10000);         // czeka na ewntualne wpisanie "dane"
+
+  
+  if (Serial.available() > 0)
   {
-    Serial.println("Problem with wiring");
-    while (1);
+    String text = Serial.readString();
+    Serial.print(text);
+    if (text == ("dane\n"))     //sprawdza czy wpisane zostało "dane"
+    {
+      Serial.print("DANE");
+      digitalWrite(37, HIGH);
+      readFile("/data.txt");                //Czyta z pamięci flash
+
+      delay(10000);
+
+      off = true;
+
+      if (Serial.available() > 0)
+      {
+        text = Serial.readString();
+        if (text == ("usun\n")){
+          deleteFile(LittleFS, "/all.csv");
+          delay(100);
+      }}
+      digitalWrite(37, LOW);
+    }
   }
 
 myScale.setGain(NAU7802_GAIN_128); //Gain can be set to 1, 2, 4, 8, 16, 32, 64, or 128.
 
-  myScale.setSampleRate(NAU7802_SPS_320); //Sample rate can be set to 10, 20, 40, 80, or 320Hz
+myScale.setSampleRate(NAU7802_SPS_320); //Sample rate can be set to 10, 20, 40, 80, or 320Hz
 
-  myScale.calibrateAFE(); //Does an internal calibration. Recommended after power up, gain changes, sample rate changes, or channel changes.
-
-   // Check if the file already exists to prevent overwritting existing data
-   bool fileexists = LittleFS.exists("/data.txt");
-   Serial.print(fileexists);
-   if(!fileexists) {
-       Serial.println("File doesn’t exist");
-       Serial.println("Creating file...");
-       // Create File and add header
-       writeFile("/data.txt", "Hamownia \r\n");
-   }
-   else {
-       Serial.println("File already exists");
-   }
-   readFile("/data.txt");
-}
-void loop() {
-  mydata = myScale.getReading();
-  appendFile("/data.txt", (String(mydata)+ "\r\n").c_str()); //Append data to the file
+myScale.calibrateAFE(); //Does an internal calibration. Recommended after power up, gain changes, sample rate changes, or channel changes.
+  // Take 10 readings to flush out readings
   
+
+  while(off){
+    delay (10);
+  }
+
+}
+
+void loop() {
+
+  while (! myScale.available()) {
+    delay(1);
+  }
+
+  int32_t val = myScale.getReading();
+  val = val/4.391825;
+  aktualny_czas = millis();
+  Serial.print("Read "); Serial.print(val); Serial.print(" czas "); Serial.println(aktualny_czas);
+
+      pakiecik2[currentIndex].zapamietanyCzas2 = aktualny_czas;
+      pakiecik2[currentIndex].nacisk = val;
+      currentIndex++;
+
+    if (currentIndex >= 12000) {
+    
+  
+    File file = LittleFS.open("/all.csv", "a");
+    digitalWrite(6, HIGH); 
+
+   for (int i = 0; i < 12000; i++) {
+       
+       memcpy(tempBuffer, &pakiecik2[i], sizeof(DataFrame));
+
+        file.write(tempBuffer, sizeof(DataFrame));
+    }
+
+    file.close();
+    digitalWrite(6, LOW);
+    currentIndex = 0;
+  }
 }
